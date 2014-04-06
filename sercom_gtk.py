95d35659a550a49964578c4bcef1450b712093ac
DeltaNova <git@deltanova.co.uk>
2014-04-06 21:19:29 +0100

    sercom_gtk.py - Improvements to GTK Window Layout

--- sercom_gtk.py
+++ sercom_gtk.py
@@ -3,11 +3,26 @@
 sercom_gtk.py - GTK Application for serial communications
 
 """
-from gi.repository import Gtk
+from gi.repository import Gtk,Gio
 
 class MyWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self,title="Sercom_Gtk")
+        self.set_border_width(0)
+        self.set_default_size(400,200)
+
+        hb = Gtk.HeaderBar()
+        hb.props.show_close_button = True
+        hb.props.title = "Sercom_Gtk"
+        self.set_titlebar(hb)
+
+        button = Gtk.Button()
+        icon = Gio.ThemedIcon(name="emblem-system-symbolic")
+        image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
+        button.add(image)
+        hb.pack_end(button)
+
+        self.add(Gtk.TextView())
 
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
