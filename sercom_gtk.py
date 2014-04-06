8372c643531767069099566b188f49869349e810
DeltaNova <git@deltanova.co.uk>
2014-04-06 18:50:34 +0100

    sercom_gtk.py - Fixed basic window generation

--- sercom_gtk.py
+++ sercom_gtk.py
@@ -6,7 +6,8 @@ sercom_gtk.py - GTK Application for serial communications
 from gi.repository import Gtk
 
 class MyWindow(Gtk.Window):
-    Gtk.Window.__init__(self,title="Sercom_Gtk")
+    def __init__(self):
+        Gtk.Window.__init__(self,title="Sercom_Gtk")
 
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
