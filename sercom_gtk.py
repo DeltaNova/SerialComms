b9b838dad56df6b7b9cdc2aeced04405d6a40c1b
DeltaNova <git@deltanova.co.uk>
2014-04-14 23:15:45 +0100

    sercom_gtk.py - Pylint - Added print_function import

--- sercom_gtk.py
+++ sercom_gtk.py
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
+from __future__ import print_function
 from gi.repository import Gtk, Gio
 
 class MyWindow(Gtk.Window):
