869ecabe5c8e3daa0c85443848027f8b225e47d6
DeltaNova <git@deltanova.co.uk>
2014-04-14 22:29:31 +0100

    Modify GTK interface design - sercom_gtk.py

--- sercom_gtk.py
+++ sercom_gtk.py
@@ -31,23 +31,31 @@ class MyWindow(Gtk.Window):
         baud_label = Gtk.Label("baud")
         port_label = Gtk.Label("port")
 
+
+        button4 = Gtk.RadioButton.new_from_widget(None)
+        button5 = Gtk.RadioButton.new_from_widget(button4)
+        button4.set_label("ASCII")
+        button5.set_label("HEX")
+        button4.connect("toggled", self.on_button_toggled, "1")
+        button5.connect("toggled", self.on_button_toggled, "2")
+
         stack = Gtk.Stack()
         stack.set_transition_type(Gtk.StackTransitionType.CROSSFADE)
         stack.set_transition_duration(500)
         stack.set_hexpand(True)
 
         self.entry = Gtk.Entry()
-        self.entry.connect("changed", self.on_ascii_change)
-        self.entry.set_text("Enter ASCII Text")
-        stack.add_titled(self.entry, "entryA","ASCII")
+        self.entry.connect("changed", self.on_entry_change)
+        #self.entry.set_text("Enter ASCII Text")
+        #stack.add_titled(self.entry, "entryA","ASCII")
 
-        self.entry2 = Gtk.Entry()
-        self.entry2.connect("changed", self.on_hex_change)
-        self.entry2.set_text("Enter HEX String")
-        stack.add_titled(self.entry2, "entryH", "HEX")
+        #self.entry2 = Gtk.Entry()
+        #self.entry2.connect("changed", self.on_hex_change)
+        #self.entry2.set_text("Enter HEX String")
+        #stack.add_titled(self.entry2, "entryH", "HEX")
 
-        stack_switcher = Gtk.StackSwitcher()
-        stack_switcher.set_stack(stack)
+        #stack_switcher = Gtk.StackSwitcher()
+        #stack_switcher.set_stack(stack)
 
         self.vbox = Gtk.VBox(0,1)
         self.add(self.vbox)
@@ -58,9 +66,12 @@ class MyWindow(Gtk.Window):
 
         self.vbox.pack_start(tv,1,1,0)
         self.vbox.pack_start(hbox2,0,0,0)
-        self.vbox.pack_start(hbox,0,0,0)
-        hbox2.pack_start(stack,1,1,0)
-        hbox.pack_start(stack_switcher,1,1,0)
+        self.vbox.pack_start(hbox,0,0,1)
+        #hbox2.pack_start(stack,1,1,0)
+        hbox2.pack_start(self.entry,1,1,0)
+        #hbox.pack_start(stack_switcher,1,1,0)
+        hbox.pack_start(button4,0,0,0)
+        hbox.pack_start(button5,0,0,0)
         hbox.pack_start(baud_label,1,1,0)
         hbox.pack_start(port_label,1,1,0)
         hbox.pack_start(button3,0,0,10)
@@ -72,10 +83,16 @@ class MyWindow(Gtk.Window):
         print("Send Button Clicked")
     def on_click_clear(self, button):
         print("Clear Button Clicked")
-    def on_ascii_change(self, entry):
-        print("ASCII Change")
+    def on_entry_change(self, entry):
+        print("Text Entry Change")
     def on_hex_change(self, entry):
         print("Hex Change")
+    def on_button_toggled(self, button, name):
+        if button.get_active():
+            state="on"
+        else:
+            state="off"
+        print("Button", name, "was turned", state)
 
 
 win = MyWindow()
